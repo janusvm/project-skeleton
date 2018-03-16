4d2586669766bcb5eca1ddb89291c628191b256e
Janus Valberg-Madsen <janus@math.aau.dk>
2018-03-16 10:59:16 +0100

    Added (almost empty) init-paper script

---
+++ init-project.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Initialise project
+
+
+# Read project info from user input
+printf "Project name (will be used for folder name and document title): "
+read PROJECT_NAME
+printf "Author(s) (seperate with commas): "
+read PROJECT_AUTHORS
+
+# TODO: rest of script...
